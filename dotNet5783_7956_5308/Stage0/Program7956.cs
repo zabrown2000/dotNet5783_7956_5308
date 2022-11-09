221c88294b059e50ab581a072b40515bf744eac3
zabrown2000 <zabrown2000@gmail.com>
2022-11-09 22:11:15 +0200

    did part 5 of stage 0

---
+++ dotNet5783_7956_5308/Stage0/Program7956.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7956();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5308();
+
+        private static void Welcome7956()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
