4027a5327a9d41b374c5078f19339a9e8d280f54
Shira Pahmer <spahmer@gmail.com>
2022-11-23 10:45:48 +0200

    tried starting stage1

---
+++ dotNet5783_7956_5308/DalFacade/DO/Enums.cs
@@ -0,0 +1,8 @@
+﻿namespace DO;
+
+public Enums{
+public enum Category {b,a };
+
+public enum OrderStatus { };
+
+}
---
+++ dotNet5783_7956_5308/DalFacade/DO/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace DO;
+
+public struct Products
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public Category Category {get; set;}
+    public int Amount { get; set; }
+    //need to add the in stock thing
+    }
