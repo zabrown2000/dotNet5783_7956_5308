1f2d8ca49e4228dff978a9712436753901d9ce21
Shira Pahmer <spahmer@gmail.com>
2023-01-20 16:21:07 +0200

    added some new windows to extend PL capability

---
+++ dotNet5783_7956_5308/PL/OpeningWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderIDWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new OrderIDWindow().ShowDialog();
+            Close();//close this window
+        }
+
+        private void openCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            new CatalogWindow().ShowDialog();
+            Close();//close this window
+        }
+    }
+}
---
+++ dotNet5783_7956_5308/PL/OrderIDWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for OrderIDWindow.xaml
+    /// </summary>
+    public partial class OrderIDWindow : Window
+    {
+        public OrderIDWindow()
+        {
+            InitializeComponent();
+        }
+        private void tid_previewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);//only gets numbers for id
+        }
+
+        private void keyPressed(object sender, TextCompositionEventArgs e)
+        {
+            //go to order tracking
+        }
+    }
+}
